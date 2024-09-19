0524959985411d88d3d71553064397fdcb9f8c89
andriana-tomova <andriana.tomova@gmail.com>
2024-09-18 20:07:21 -0400

    Add Footer and number of pages in each section according to the data in .csv file. Add lines on each page

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4" )
-
+pdf.set_auto_page_break(auto=False, margin=0)
 df = pd.read_csv("topics.csv")
 
 print(df)
@@ -13,7 +13,27 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=10)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"],align="L", ln=1)
-    pdf.line(10, 22, 200, 22)
+    for i in range(22, 288, 6):
+        pdf.line(10, i, 200, i)
     print(index, row["Topic"])
+
+    # Set footer
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="B", size=10)
+    pdf.set_text_color(180, 180, 100)
+    pdf.cell(w=0, h=12, txt=row["Topic"],align="R", ln=1)
+
+
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
+
+        # Set footer
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="B", size=10)
+        pdf.set_text_color(180, 180, 100)
+        pdf.cell(w=0, h=12, txt=row["Topic"], align="R", ln=1)
+        for i in range(22, 288, 6):
+            pdf.line(10, i, 200, i)
+
 
 pdf.output("output.pdf")
